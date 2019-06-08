7ae825b8f94ca11eb3caae9b33b3db18a1717840
Michael Hazzard <miramardesign@gmail.com>
2019-06-08 16:20:45 -0300

    Updates

---
+++ precache-manifest.0be36c036b3ac89f662e28770077b25d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d853f46afcb7cdd54dd9703778b48950",
+    "url": "/seven-wonders-duel-scorecard-react/index.html"
+  },
+  {
+    "revision": "fc8233067f0a87803e4e",
+    "url": "/seven-wonders-duel-scorecard-react/static/css/2.482555ae.chunk.css"
+  },
+  {
+    "revision": "49e762d4396804873c3f",
+    "url": "/seven-wonders-duel-scorecard-react/static/css/main.5983c323.chunk.css"
+  },
+  {
+    "revision": "fc8233067f0a87803e4e",
+    "url": "/seven-wonders-duel-scorecard-react/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "49e762d4396804873c3f",
+    "url": "/seven-wonders-duel-scorecard-react/static/js/main.ff5dba8f.chunk.js"
+  },
+  {
+    "revision": "3a6414b0d55870686d16",
+    "url": "/seven-wonders-duel-scorecard-react/static/js/runtime~main.2f0f56e1.js"
+  }
+]);
